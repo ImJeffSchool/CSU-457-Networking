aa8628c6905ff65e6001fec07d26bc1b6207e75a
Chris Beall <cjbeall@hotmail.com>
2024-10-02 18:54:17 -0600

    possible error handling

--- TCPClient.py
+++ TCPClient.py
@@ -12,17 +12,23 @@ import TCPServer
 Static_HOST = '0.0.0.0'
 Static_PORT = 54321
 
+logging.basicConfig(filename='Client.log', level=logging.INFO)
 
 sel = selectors.DefaultSelector()
 
 
 def startConnection(Static_HOST, Static_PORT):
     serverAddress = (Static_HOST, Static_PORT)
-    print("starting connection to", serverAddress)
-    logging.basicConfig(filename='Client.log', level=logging.INFO)
+    print('Starting connection to ', serverAddress)
+    logging.info('Starting connection to ', serverAddress)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(False)
-    sock.connect_ex(serverAddress)
+    errorNumber = sock.connect_ex(serverAddress)
+    if errorNumber == 0 :
+        print("IDK WHAT TO PUT HERE")
+    elif errorNumber == 1:
+        print('Unable to connect. Error code: ', errorNumber)
+        logging.info('Unable to connect. Error code: ', errorNumber)
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     message1 = input("Enter a message you would like to send to the server")
     message1.encode()
@@ -31,7 +37,7 @@ def startConnection(Static_HOST, Static_PORT):
     messages = [message1, message2]
     sel.register(sock, events, data = messages)
     
-    
+
 startConnection(Static_HOST, Static_PORT)
 
 
