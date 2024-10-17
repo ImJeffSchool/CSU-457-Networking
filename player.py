a3ff191a1785aa5100cf6f34c78feff33d1cce18
Benjamin Ayala <bigben.design@outlook.com>
2024-10-17 11:49:23 -0600

    Updated w/ toggleReady func

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player:
         self.request_queued = False
         self.request = None
         self.response = None
+        self.isReady = False
 
     def _addPoints(self, points):
         self.points += points
@@ -14,6 +15,10 @@ class Player:
 
     def askQuestion(self, questionVal):
         # need to fill out for json
+
+    def toggleReady(self):
+        if self.isReady is False: self.isReady = True
+        else: self.isReady = False
 
     def answerQuestion(self, answer):
         # need to fill out for json
