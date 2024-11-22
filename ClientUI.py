f52dc7c3b987c13c7b6419a7d3e3f829f2624fb8
Chris Beall <cjbeall@hotmail.com>
2024-11-22 12:44:19 -0700

    added text input to client ready button

--- ClientUI.py
+++ ClientUI.py
@@ -11,19 +11,14 @@ class ClientUI:
                 [800, 800, 800, 800, 800],
                 [1600, 1600, 1600, 1600, 1600]
             ]
+    
     def quitButtonOnClick(self):
         #sel.unregister
         #sock.close()
         #IDK if the above happens here i just have this in a function incase we want to handle it here
         root.destroy()
 
-    def makeBoard(self, root, buttonFrame):
-        for i in range(len(self.displayBoard)):
-            for j in range(len(self.displayBoard)):
-                button = tk.Button(buttonFrame, text=self.displayBoard[i][j], font=('Arial', 32))
-                button.grid(row=i, column=j+1, padx=10, pady=20)
-    
-    def startButtonOnClick(self):
+    def makeBoard(self):
         root = tk.Tk()
         root.geometry("1000x1000")
         root.title("Jeopardy")
@@ -35,11 +30,42 @@ class ClientUI:
         buttonFrame.columnconfigure(0, weight=1)
         buttonFrame.columnconfigure(1, weight=1)
         buttonFrame.pack()
-
-        self.makeBoard(root, buttonFrame)
+        
+        for i in range(len(self.displayBoard)):
+            for j in range(len(self.displayBoard)):
+                button = tk.Button(buttonFrame, text=self.displayBoard[i][j], font=('Arial', 32))
+                button.grid(row=i, column=j+1, padx=10, pady=20)
+    
+    def get_input(self, entry):
+        input = entry.get()
+        return input
+    
+    def startButtonOnClick(self):
+        self.createReadyUI()
+        #self.makeBoard()
     
     def helpButtonOnClick(self):
         messagebox.showinfo("Message","HELP I AM TRAPPED IN HERE")
+        
+    def createReadyUI(self):
+        root = tk.Tk()
+        root.geometry("1000x1000")
+        root.title("nameSubmission")
+        label = tk.Label(root, text="Please enter your name:", font=('Arial', 48))
+        label.pack()
+        
+        buttonFrame = tk.Frame(root)
+        buttonFrame.columnconfigure(0, weight=1)
+        buttonFrame.columnconfigure(1, weight=1)
+        buttonFrame.pack()
+        
+        entry = tk.Entry(root)
+        entry.pack()
+        
+        
+        submitButton = tk.Button(root, text = "Submit", command=lambda: self.get_input(entry))
+        submitButton.pack()
+        
     def __init__(self):
         #root window for UI
         root = tk.Tk()
