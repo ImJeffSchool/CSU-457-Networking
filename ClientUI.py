a1f30503e0d034d0e6b3438300628c550a80ebf0
Chris Beall <cjbeall@hotmail.com>
2024-11-22 12:21:13 -0700

    inital hookup to client

--- ClientUI.py
+++ ClientUI.py
@@ -11,22 +11,35 @@ class ClientUI:
                 [800, 800, 800, 800, 800],
                 [1600, 1600, 1600, 1600, 1600]
             ]
+    def quitButtonOnClick(self):
+        #sel.unregister
+        #sock.close()
+        #IDK if the above happens here i just have this in a function incase we want to handle it here
+        root.destroy()
+
+    def makeBoard(self, root, buttonFrame):
+        for i in range(len(self.displayBoard)):
+            for j in range(len(self.displayBoard)):
+                button = tk.Button(buttonFrame, text=self.displayBoard[i][j], font=('Arial', 32))
+                button.grid(row=i, column=j+1, padx=10, pady=20)
     
-    # def startButtonOnClick():
-    #     root = tk.Tk()
-    #     root.geometry("1000x1000")
-    #     root.title("Jeopardy")
+    def startButtonOnClick(self):
+        root = tk.Tk()
+        root.geometry("1000x1000")
+        root.title("Jeopardy")
 
-    #     label = tk.Label(root, text="Welcome to the Jeopardy game!", font=('Arial', 48))
-    #     label.pack()
+        label = tk.Label(root, text="Welcome to the Jeopardy game!", font=('Arial', 48))
+        label.pack()
 
-    #     buttonFrame = tk.Frame(root)
-    #     buttonFrame.columnconfigure(0, weight=1)
-    #     buttonFrame.columnconfigure(1, weight=1)
-    #     buttonFrame.pack()
+        buttonFrame = tk.Frame(root)
+        buttonFrame.columnconfigure(0, weight=1)
+        buttonFrame.columnconfigure(1, weight=1)
+        buttonFrame.pack()
 
-    #     makeBoard(root, buttonFrame)
+        self.makeBoard(root, buttonFrame)
     
+    def helpButtonOnClick(self):
+        messagebox.showinfo("Message","HELP I AM TRAPPED IN HERE")
     def __init__(self):
         #root window for UI
         root = tk.Tk()
@@ -44,33 +57,20 @@ class ClientUI:
         buttonFrame.pack()
 
         # Buttons are placed in the frame and any placement modifiers will be done w/in the buttonFrame OBJ
-        #readyBtn = tk.Button(buttonFrame, text="Ready Up", font=('Arial', 16), command=startButtonOnClick)
-        readyBtn = tk.Button(buttonFrame, text="Ready Up", font=('Arial', 16), command=None)
+        readyBtn = tk.Button(buttonFrame, text="Ready Up", font=('Arial', 16), command=self.startButtonOnClick)
+        #readyBtn = tk.Button(buttonFrame, text="Ready Up", font=('Arial', 16), command=None)
         readyBtn.grid(row=0, column=0)
-        #quitBtn = tk.Button(buttonFrame, text="Quit", font=('Arial', 16), command=quitButtonOnClick)
-        quitBtn = tk.Button(buttonFrame, text="Quit", font=('Arial', 16), command=None)
+        quitBtn = tk.Button(buttonFrame, text="Quit", font=('Arial', 16), command=self.quitButtonOnClick)
+        #quitBtn = tk.Button(buttonFrame, text="Quit", font=('Arial', 16), command=None)
         quitBtn.grid(row=2, column=0)
-        #helpBtn = tk.Button(buttonFrame, text="Help", font=('Arial', 16), command=helpButtonOnClick)
-        helpBtn = tk.Button(buttonFrame, text="Help", font=('Arial', 16), command=None)
+        helpBtn = tk.Button(buttonFrame, text="Help", font=('Arial', 16), command=self.helpButtonOnClick)
+        #helpBtn = tk.Button(buttonFrame, text="Help", font=('Arial', 16), command=None)
         helpBtn.grid(row=1, column=0)
 
         # Called after all elements have been created and packed
         root.mainloop()
 
-    def helpButtonOnClick():
-        messagebox.showinfo("Message","HELP I AM TRAPPED IN HERE")
 
-    # def quitButtonOnClick():
-    #     #sel.unregister
-    #     #sock.close()
-    #     #IDK if the above happens here i just have this in a function incase we want to handle it here
-    #     root.destroy()
-
-    # def makeBoard(root, buttonFrame):
-    #     for i in range(len(displayBoard)):
-    #         for j in range(len(displayBoard)):
-    #             button = tk.Button(buttonFrame, text=displayBoard[i][j], font=('Arial', 32))
-    #             button.grid(row=i, column=j+1, padx=10, pady=20)
 
 
 
